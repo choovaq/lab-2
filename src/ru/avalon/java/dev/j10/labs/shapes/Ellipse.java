d50e751cbfa6474effb9fb0f41cb2916f5c5f634
choovaq <abrsnv@gmail.com>
2019-03-30 10:29:17 +0300

    Add files via upload

--- src/ru/avalon/java/dev/j10/labs/shapes/Ellipse.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Ellipse.java
@@ -10,7 +10,7 @@ package ru.avalon.java.dev.j10.labs.shapes;
  *
  * @see <a href="https://ru.wikipedia.org/wiki/%D0%AD%D0%BB%D0%BB%D0%B8%D0%BF%D1%81">Эллипс</a>
  */
-public interface Ellipse {
+public interface Ellipse{
 
     /*
      * TODO: Закончить определение интерфейса 'Ellipse'
--- src/ru/avalon/java/dev/j10/labs/shapes/Rectangle.java
+++ src/ru/avalon/java/dev/j10/labs/shapes/Rectangle.java
@@ -8,8 +8,37 @@ package ru.avalon.java.dev.j10.labs.shapes;
  *
  * @see <a href="https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D1%8F%D0%BC%D0%BE%D1%83%D0%B3%D0%BE%D0%BB%D1%8C%D0%BD%D0%B8%D0%BA">Прямоугольник</a>
  */
-public class Rectangle {
+public class Rectangle implements Polygon, Shape{
 
+    private final float a;
+    private final float b;
+    private final int rotation;
+
+    public Rectangle() {
+        a = (float) (10 * Math.random());
+        b = (float) (10 * Math.random());
+        rotation = (int) (10 * Math.random());
+    }
+    
+    
+    public float getPerimeter() {
+        return 2 * (a + b);
+    }
+
+    
+    public float getArea() {
+        return a * b;
+    }
+
+    
+    public int getRotation() {
+        return rotation;  
+    }
+   
+    public String toString() {
+        return "Rectangle";
+    }
+    
     /*
      * TODO: Реализовать класс 'Rectangle'
      * 1. Используйте наследование.
